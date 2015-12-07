429995b4628e7d72b50918508fbe40002657cdf9
Mike Soulia <mike.soulia@gmail.com>
2015-12-07 19:08:46 +0000

    mv /home/ubuntu/workspace/client /home/ubuntu/workspace/app

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var express = require('express');
 var router = express();
 var server = http.createServer(router);
 
-router.use(express.static(path.resolve(__dirname, 'client')));
+router.use(express.static(path.resolve(__dirname, 'app')));
 // var messages = [];
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
