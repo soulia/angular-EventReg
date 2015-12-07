cae5b3d10e01f2de26d34d4172d694894ca5cc24
Mike Soulia <mike.soulia@gmail.com>
2015-12-07 22:07:38 +0000

    organized modules, adjusted server.js to handle a non-default index.html

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,15 +1,24 @@
+// IIFE pattern
+
 (function() {
 
     // 2/3 - define the eventsApp module. It's important to pass the [] which tells angular that the module is defined here,
     // otherwise, angular will think that this is a reference to a module that is defined elsewhere.
-    var eventsApp = angular.module("eventViewer", []);
+    // var eventsApp = angular.module("eventViewer", []);
+    eventsApp = angular.module("eventsApp");
 
     var EventController = function($scope) {
 
         $scope.event = {
             name: 'Angular Boot Camp',
             date: '12/4/2015',
-            time: ' 4:20 pm'
+            time: ' 4:20 pm',
+            location: {
+                address: 'Smuggs',
+                city: 'Cambridge',
+                state: 'VT'
+            },
+            imageUrl: '/img/angularjs-logo.png'
         };
 
     };
--- server.js
+++ server.js
@@ -1,11 +1,5 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
 var http = require('http');
 var path = require('path');
-
 var express = require('express');
 
 //
@@ -14,13 +8,14 @@ var express = require('express');
 // Creates a new instance of SimpleServer with the following options:
 //  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
 //
-var router = express();
-var server = http.createServer(router);
+var app = express();
+var server = http.createServer(app);
 
-router.use(express.static(path.resolve(__dirname, 'app')));
-// var messages = [];
+// http://stackoverflow.com/questions/33818217/node-js-how-to-make-default-page-to-be-sth-other-than-index-html
+// app.use(express.static(path.resolve(__dirname, 'app')));  // this defaults to index.html
+app.use(express.static(path.resolve(__dirname, 'app'), {index: 'EventDetails.html'}));
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
+  console.log("Event Reg listening at", addr.address + ":" + addr.port);
 });
